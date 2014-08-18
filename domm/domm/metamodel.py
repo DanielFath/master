c864b5daf0155b3213cf3d917135bdc460813685
Daniel Fath <daniel.fath7@gmail.com>
2014-08-18 05:48:52 +0200

    Attempt fix broken build

--- domm/domm/metamodel.py
+++ domm/domm/metamodel.py
@@ -1176,8 +1176,9 @@ class Service(NamedElement, NamespacedObject):
 
     def add_op_compartment(self, compartment):
         self.op_compartments[compartment.name] = compartment
-        for val in self.op_compartments.elements:
-            self.elems[val.type_def.name] = val
+        for compartment in self.op_compartments:
+            for val in compartment.elements:
+                self.elems[val.type_def.name] = val
         return self
 
     def __repr__(self):
