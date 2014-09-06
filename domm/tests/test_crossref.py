081612c1a07422cfa5292482c409e46346d15dc5
Daniel Fath <daniel.fath7@gmail.com>
2014-09-06 18:04:29 +0200

    Add test stub for crossref

---
+++ domm/tests/test_crossref.py
@@ -0,0 +1,12 @@
+##############################################################################
+# Name: test_crossref.py
+# Purpose: Test for verifying output the semantic verification of cross-refs
+# Author: Daniel Fath <daniel DOT fath7 AT gmail DOT com>
+# Copyright: (c) 2014 Daniel Fath <daniel DOT fath7 AT gmail DOT com>
+# License: MIT License
+##############################################################################
+from  domm.parser import DommParser
+from  domm.metamodel import *
+
+def test_model():
+    pass
